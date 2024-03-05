a163e2f6810184355356344ed72a9d66b997a0ca
Frederic Bec <bec.frederic90@outlook.fr>
2024-03-05 14:16:50 +0100

    Add order component

---
+++ src/app/components/order/order.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Customer } from 'src/app/model/customer.model';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.css']
+})
+export class OrderComponent implements OnInit {
+
+  dateOrder : Date = new Date();
+  customer : Customer | undefined;
+
+  constructor(public cartService : CartService, private router : Router) { }
+
+  ngOnInit(): void {
+    this.customer = this.cartService.getCustomer();
+  }
+
+  onOrder(){
+    if(confirm("Merci de votre visite : ")){
+      this.cartService.clearLocalStorage();
+      this.router.navigateByUrl('');
+    }
+  }
+
+}
