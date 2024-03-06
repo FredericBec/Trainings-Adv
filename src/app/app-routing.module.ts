76c6212a0578402bb905f31a3d63e8dc64f435bc
Frederic Bec <bec.frederic90@outlook.fr>
2024-03-06 11:59:23 +0100

    Add login import and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { CartComponent } from './components/cart/cart.component';
 import { CustomerComponent } from './components/customer/customer.component';
 import { OrderComponent } from './components/order/order.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path : 'trainings', component : TrainingsComponent},
   { path : 'cart' , component : CartComponent },
   {path : 'customer', component : CustomerComponent},
   {path : 'order', component : OrderComponent},
+  {path : 'login', component : LoginComponent},
   {path : '', redirectTo : 'trainings', pathMatch : 'full'},
   {path : '404', component : PageNotFoundComponent},
   {path : '**', redirectTo : '404'}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TrainingsComponent } from './components/trainings/trainings.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { CartComponent } from './components/cart/cart.component';
 import { CustomerComponent } from './components/customer/customer.component';
 import { OrderComponent } from './components/order/order.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -18,13 +19,15 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
     CartComponent,
     CustomerComponent,
     OrderComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
