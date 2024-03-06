9fa058a1f8928c9d6658136467fd7619b868b0eb
Frederic Bec <bec.frederic90@outlook.fr>
2024-03-06 15:45:51 +0100

    Add getUser into formbuilder

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -12,10 +12,10 @@ import { AuthService } from 'src/app/services/auth.service';
 export class LoginComponent implements OnInit {
 
   userForm : FormGroup
-  constructor(private formBuilder : FormBuilder, private authService : AuthService, private router : Router) { 
+  constructor(private formBuilder : FormBuilder, public authService : AuthService, private router : Router) { 
     this.userForm = this.formBuilder.group({
-      email: ['', Validators.required],
-      password: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9.@]*')]]
+      email: [this.authService.getUser().email, Validators.required],
+      password: [this.authService.getUser().password, [Validators.required, Validators.pattern('[a-zA-Z0-9.@]*')]]
     });
   }
 
