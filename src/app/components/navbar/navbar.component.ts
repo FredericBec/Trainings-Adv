1d6a17a5ad8129980759c699052ebe941e2fcfe7
Frederic Bec <bec.frederic90@outlook.fr>
2024-03-07 11:58:14 +0100

    Add admin link

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   isUserLoggedIn = false;
-  constructor() { }
+  constructor(public authService : AuthService) { 
+  }
 
   ngOnInit(): void {
     let storeData = localStorage.getItem('isLoggedIn');
